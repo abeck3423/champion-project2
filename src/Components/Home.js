ed9a8ba223ef85f2f9871694e000c04fe5449294
Alexander Becker <alexbecker@Alexanders-MacBook-Pro.local>
2022-06-14 11:23:19 -0700

    Created home component to call AllChamps component

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "./react";
+import AllChamps from "./AllChamps";
+
+function Home(props) {
+  return (
+    <div>
+      <h1 className="homeIntro">Choose your Champion!</h1>
+      <div className="homePage">
+        <AllChamps onChange={props.onChange} />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
