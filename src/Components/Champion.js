aa8bff2b7cbb0f3682c510e21a9a1dbbebece7e8
Alexander Becker <alexbecker@Alexanders-MacBook-Pro.local>
2022-06-14 11:29:57 -0700

    Updated Champion.js component include ChampDetails props.

---
+++ src/Components/Champion.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ChampDetails from "./ChampDetails.js";
+
+function Champion(props) {
+  return (
+    <div>
+      {props.champion ? (
+        <ChampDetails champion={props.champion} onChange={props.onChange} />
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
+}
+
+export default Champion;
